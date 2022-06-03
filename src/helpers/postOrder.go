3d9812c12057b8321df69425be3e15c067a05fb3
DevYadav-RaRa <dev.yadav@rara.delivery>
2022-06-03 23:21:04 +0530

    Added: postOrder function

---
+++ src/helpers/postOrder.go
@@ -0,0 +1,27 @@
+package helpers
+
+import (
+	"fmt"
+
+	"github.com/RaRa-Delivery/rara-ms-boilerplate/src/models"
+)
+
+func postOrder(order models.OrderObject, header string) (string, bool) {
+	IamAuth := order.GetIamAuthentication(header)
+
+	if IamAuth.Status {
+		order.OrderDetails.Status = "Processing"
+		// 	Save mongo
+		err, status := order.Insert()
+		if err != nil {
+			return err.Error(), status
+		}
+		// 	Send it to the queue -> Order acceptence
+		fmt.Println("Pushing to Queue")
+		fmt.Println("Pushed to Queue")
+
+		return "Success: Processing", true
+	}
+	order.OrderDetails.Status = "Rejected"
+	return "Invalid: Iam Rejected", false
+}
