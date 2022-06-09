6b8131879bf9b3b38146375ce3970553d9cb06d7
DevYadav-RaRa <dev.yadav@rara.delivery>
2022-06-09 12:36:23 +0530

    Updated: REST Api response

--- src/restApis/orderRest.go
+++ src/restApis/orderRest.go
@@ -1,6 +1,8 @@
 package restApis
 
 import (
+	"errors"
+
 	"github.com/RaRa-Delivery/rara-ms-boilerplate/src/conf"
 	"github.com/RaRa-Delivery/rara-ms-boilerplate/src/framework"
 	routing "github.com/qiangxue/fasthttp-routing"
@@ -9,7 +11,7 @@ import (
 func InitApis(appCtx framework.Framework) {
 
 	appCtx.Router.Post("/", func(c *routing.Context) error {
-		conf.ConsumeApiOrders(string(c.PostBody()))
-		return nil
+		resp, _ := conf.ConsumeApiOrders(string(c.PostBody()))
+		return errors.New(resp)
 	})
 }
